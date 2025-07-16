b114460d7e18ff6390f0122eb95225d32105c28f
tristancable <tcable@student.neumont.edu>
2025-07-16 12:50:30 -0600

    created main menu with play, settings, and quit button functionality

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,6 +5,11 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
