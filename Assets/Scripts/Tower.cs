f5ae951bd46dc6b20b54891cb0613a687ef7b1dc
Xenny76 <dredford@student.neumont.edu>
2025-07-21 14:33:59 -0600

    more tower progress

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
@@ -6,9 +7,20 @@ public class Tower : MonoBehaviour
     [SerializeField] private float fireRate;
     [SerializeField] private float damage;
     [SerializeField] private byte targetCount;
-    // Update is called once per frame
+    [SerializeField] private CircleCollider2D rangeCollider;
+    private List<Collider2D> targetsInRange = new();
+    private Queue<Enemy> targetsToShoot = new();
+    void Start()
+    {
+        rangeCollider.radius = range;
+        rangeCollider.enabled = true;
+    }
     void Update()
     {
-        
+        rangeCollider.Overlap(new ContactFilter2D().NoFilter(), targetsInRange);
+        if (targetsInRange.Count > 0)
+        {
+
+        }
     }
 }
