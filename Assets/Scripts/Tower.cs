3753db643f32b95d0d007245b2786e77b1e3943f
Xenny76 <dredford@student.neumont.edu>
2025-07-22 09:49:54 -0600

    beams added to towers

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,6 +22,10 @@ public class Tower : MonoBehaviour
     [SerializeField] private double fireRate;
     [SerializeField] private double damage;
     [SerializeField] private byte targetCount;
+    [Header("Beam Effect")]
+    [SerializeField] private Material beamMaterial;
+    [SerializeField] private float beamWidth;
+    [SerializeField] private float beamDuration;
     private CircleCollider2D rangeCollider;
     private List<Collider2D> targetsInRange = new();
     private float fireCooldown = 0f;
@@ -59,7 +64,6 @@ public class Tower : MonoBehaviour
         switch (priority)
         {
             case TargetPriority.Random:
-                // Fisher–Yates shuffle
                 for (int i = targetsInRange.Count - 1; i > 0; i--)
                 {
                     int j = UnityEngine.Random.Range(0, i + 1);
@@ -93,7 +97,6 @@ public class Tower : MonoBehaviour
                 break;
 
             case TargetPriority.MostProgressed:
-                // assumes your Enemy has a public float Progress [0..1] or waypointIndex
                 targetsInRange.Sort((a, b) =>
                     b.GetComponent<Enemy>().waypointIndex
                      .CompareTo(a.GetComponent<Enemy>().waypointIndex));
@@ -114,9 +117,41 @@ public class Tower : MonoBehaviour
         int shots = Mathf.Min(targetCount, targetsInRange.Count);
         for (int i = 0; i < shots; i++)
         {
-            var e = targetsInRange[i].GetComponent<Enemy>();
-            if (e != null) e.TakeDamage(damage);
+            var enemyCollider = targetsInRange[i];
+            var enemy = enemyCollider.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+
+                StartCoroutine(DrawBeam(transform.position, enemyCollider.transform.position));
+            }
         }
     }
 
+    private IEnumerator DrawBeam(Vector3 from, Vector3 to)
+    {
+        // 1) create a GameObject just for this beam
+        GameObject go = new GameObject("Beam");
+        var lr = go.AddComponent<LineRenderer>();
+
+        // 2) configure its look
+        lr.positionCount = 2;
+        lr.material = beamMaterial;
+        lr.startWidth = beamWidth;
+        lr.endWidth = beamWidth;
+        lr.useWorldSpace = true;
+
+        // 3) set the two endpoints
+        lr.SetPosition(0, from);
+        lr.SetPosition(1, to);
+
+        // 4) optionally tweak sorting/order, e.g.:
+        //    lr.sortingLayerName = "Projectiles";
+        //    lr.sortingOrder     = 5;
+        lr.sortingLayerName = "Foreground";
+
+        // 5) wait, then clean up
+        yield return new WaitForSeconds(beamDuration);
+        Destroy(go);
+    }
 }
